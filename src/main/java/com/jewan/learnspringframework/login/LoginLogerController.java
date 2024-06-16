ef2e03797efcf384e1d749f0673d266ba8550a52
Jewan1120 <hawaii1468@gmail.com>
2024-06-16 15:57:13 +0900

    [rename] 파일명 수정

--- src/main/java/com/jewan/learnspringframework/login/LoginLogerController.java
+++ src/main/java/com/jewan/learnspringframework/login/LoginLogerController.java
@@ -8,12 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class LoginController {
+public class LoginLogerController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("login") // /login?name=Jewan
     public String gotoLoginPage(@RequestParam("name") String name, ModelMap model) { // String으로 파라미터 전달 받음
+        // @RequestParam("파라미터 명")의 파라미터명을 적어줘야함
+        model.put("name", name); // 모델에서 값이 뷰로 자동으로 전달 jsp에서 ${}로 값 꺼내 씀
+
+        // System.out.println("Request param is " + name); 권장되진 않는 출력 -> 로그 사용
+        logger.debug("Request param is {}", name); // debug 레벨
+        logger.info("Request param is {}", name); // info 레벨 -> 프로덕션에서 사용
+        logger.warn("Request param is {}", name); // warn 레벨
+        // Request param is jewan
+
         return "login";
     }
 }
