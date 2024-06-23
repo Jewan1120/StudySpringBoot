009461b79bffe2329b87a4ca7fca7352626ab4ef
Jewan1120 <hawaii1468@gmail.com>
2024-06-23 14:57:11 +0900

    [feat] Todo List 페이지, 서비스 작성

---
+++ src/main/java/com/jewan/learnspringframework/todo/TodoController.java
@@ -0,0 +1,26 @@
+package com.jewan.learnspringframework.todo;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        super();
+        this.todoService = todoService;
+    }
+
+    // /list-todos
+    @RequestMapping("list-todos")
+    public String listAllTodos(ModelMap model) {
+        List<Todo> todos = todoService.findByUsername("jewan");
+        model.addAttribute("todos", todos);
+        return "listTodos";
+    }
+
+}
