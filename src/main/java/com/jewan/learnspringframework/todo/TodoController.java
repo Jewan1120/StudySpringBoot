0debb0f32700292c226fa09fe4b25c70b218a6a9
Jewan1120 <hawaii1468@gmail.com>
2024-06-26 23:15:56 +0900

    [feat] Todo 서비스에 addTodo 로직 구현

--- src/main/java/com/jewan/learnspringframework/todo/TodoController.java
+++ src/main/java/com/jewan/learnspringframework/todo/TodoController.java
@@ -1,10 +1,12 @@
 package com.jewan.learnspringframework.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -33,8 +35,9 @@ public class TodoController {
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodo() {
-        
+    public String addNewTodo(@RequestParam("description") String description, ModelMap model) {
+        String username = (String) model.get("name"); // 뷰 model 안에 담겨 있던 값
+        todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
         return "redirect:list-todos"; // URL 재 요청
     }
 }
--- src/main/java/com/jewan/learnspringframework/todo/TodoService.java
+++ src/main/java/com/jewan/learnspringframework/todo/TodoService.java
@@ -9,14 +9,20 @@ import org.springframework.stereotype.Service;
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
 
+    private static int todosCount = 0;
+
     static {
-        todos.add(new Todo(1, "jewan", "taekbae", LocalDate.now(), false));
-        todos.add(new Todo(2, "jewan", "shopo", LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(3, "jewan", "doldol", LocalDate.now().plusYears(2), false));
-        todos.add(new Todo(4, "jewan", "gogo", LocalDate.now().plusYears(3), false));
+        todos.add(new Todo(++todosCount, "jewan", "taekbae", LocalDate.now(), false));
+        todos.add(new Todo(++todosCount, "jewan", "shopo", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(++todosCount, "jewan", "doldol", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(++todosCount, "jewan", "gogo", LocalDate.now().plusYears(3), false));
     }
 
     public List<Todo> findByUsername(String name) {
         return todos;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        todos.add(new Todo(++todosCount, username, description, targetDate, done));
     }
 }
