29829e72ee7239fb762918f4dc7d06649a9a4bb4
Jewan1120 <hawaii1468@gmail.com>
2024-07-08 00:41:28 +0900

    [feat] PathVariable 어노테이션으로 RestAPI에 파라미터 전달

--- src/main/java/com/jewan/learnspringframework/helloworld/HelloWorldController.java
+++ src/main/java/com/jewan/learnspringframework/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.jewan.learnspringframework.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // REST API를 사용하기 위한 어노테이션
@@ -11,9 +12,16 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
-    
+
     @GetMapping("hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    // Path Parameters
+    // {}로 감싸서 전달할 수 있음
+    @GetMapping("hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable("name") String name) {
+        return new HelloWorldBean(String.format("Hello %s", name));
     }
 }
