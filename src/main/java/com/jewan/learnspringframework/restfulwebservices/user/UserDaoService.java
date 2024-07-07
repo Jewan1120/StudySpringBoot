02c7a859812ab32fdc22c82adda5f30903a5b17f
Jewan1120 <hawaii1468@gmail.com>
2024-07-08 01:54:41 +0900

    [feat] POST를 이용해서 유저 등록 후 201 상태 반환하도록 작성 - 200 : Success - 201 : Create

--- src/main/java/com/jewan/learnspringframework/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/jewan/learnspringframework/restfulwebservices/user/UserDaoService.java
@@ -12,11 +12,12 @@ public class UserDaoService {
     // UserDaoService -> Static List
 
     private static List<User> users = new ArrayList<>();
+    private static Integer usersCount = 0;
 
     static {
-        users.add(new User(1, "Jewan", LocalDate.now().minusYears(27)));
-        users.add(new User(2, "TaekBae", LocalDate.now().minusYears(10)));
-        users.add(new User(3, "ShoPo", LocalDate.now().minusYears(6)));
+        users.add(new User(++usersCount, "Jewan", LocalDate.now().minusYears(27)));
+        users.add(new User(++usersCount, "TaekBae", LocalDate.now().minusYears(10)));
+        users.add(new User(++usersCount, "ShoPo", LocalDate.now().minusYears(6)));
     }
 
     public List<User> findAll() {
@@ -26,5 +27,11 @@ public class UserDaoService {
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
         return users.stream().filter(predicate).findFirst().get();
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
     }
 }
--- src/main/java/com/jewan/learnspringframework/restfulwebservices/user/UserResource.java
+++ src/main/java/com/jewan/learnspringframework/restfulwebservices/user/UserResource.java
@@ -1,9 +1,14 @@
 package com.jewan.learnspringframework.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class UserResource {
@@ -25,4 +30,19 @@ public class UserResource {
     public User retrieveUser(@PathVariable("id") int id) {
         return service.findOne(id);
     }
+
+    // talend api tester 확장 프로그램을 이용해서 Post 요청 전달
+
+    @PostMapping("users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        // locateion -> users/{id}
+        // created()의 인자로 URI을 받음
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build(); // 201 상태를 반환
+    }
+
 }
