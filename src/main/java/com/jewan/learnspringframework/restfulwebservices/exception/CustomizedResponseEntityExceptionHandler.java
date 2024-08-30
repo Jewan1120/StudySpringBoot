451bf52b59c9360b67658dbd1b9fe7104cc1347a
Jewan1120 <hawaii1468@gmail.com>
2024-08-30 22:14:13 +0900

    [feat] 스프링에서 제공하는 에러 핸들러를 커스터마이징

---
+++ src/main/java/com/jewan/learnspringframework/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.jewan.learnspringframework.restfulwebservices.exception;
+
+import java.time.LocalDate;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.jewan.learnspringframework.restfulwebservices.user.UserNotFoundException;
+
+@ControllerAdvice 
+// Spring Framework에서 전역적으로 예외를 처리하거나 컨트롤러에 공통된 설정을 적용할 때 사용하는 기능
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class) // 모든 에러를 처리하려고 할 떄
+    public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+        
+        // ResponseEntity : JSON 형태로 반환
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    
+    @ExceptionHandler(UserNotFoundException.class) // 특정 에러만 처리하고 싶을 때
+    public final ResponseEntity<ErrorDetails> handleUserNotFoindException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+        
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+}
