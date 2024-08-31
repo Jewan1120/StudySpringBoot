835781ccff418315113485747d4d935b78cbb52b
Jewan1120 <hawaii1468@gmail.com>
2024-08-31 23:06:42 +0900

    [feat] RestAPI에서의 동적 필터링

--- src/main/java/com/jewan/learnspringframework/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/jewan/learnspringframework/restfulwebservices/filtering/FilteringController.java
@@ -3,14 +3,29 @@ package com.jewan.learnspringframework.restfulwebservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 
-    @GetMapping("/filtering")
-    public List<SomeBean> filtering() {
+    @GetMapping("filtering")
+    public MappingJacksonValue filtering() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean); // 동적으로 필터링
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field3"); // field3만 응답으로 반환
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter); // 여러가지 필터를 정의
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
+    }
+
+    @GetMapping("/filtering-list")
+    public List<SomeBean> filteringList() {
         // 두번째 field를 생략함
         return Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("value4", "value5", "value6"));
     }
--- src/main/java/com/jewan/learnspringframework/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/jewan/learnspringframework/restfulwebservices/filtering/SomeBean.java
@@ -1,15 +1,18 @@
 package com.jewan.learnspringframework.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties({ "field1", "field4" }) // 클래스에 직접 무시할 변수 정의 가능
+//@JsonIgnoreProperties({ "field1", "field4" }) // 클래스에 직접 무시할 변수 정의 가능
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
 
     @JsonIgnore // Json에 해당 변수를 포함시키지 않음
     private String field2;
     private String field3;
+    
 
     public SomeBean(String field1, String field2, String field3) {
         super();
