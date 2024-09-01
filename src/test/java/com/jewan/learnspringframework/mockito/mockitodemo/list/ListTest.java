5b1f0438684d244b54c7e38e03a8103b330d67ab
Jewan1120 <hawaii1468@gmail.com>
2024-09-02 01:30:36 +0900

    [test] List 인터페이스 모킹

---
+++ src/test/java/com/jewan/learnspringframework/mockito/mockitodemo/list/ListTest.java
@@ -0,0 +1,47 @@
+package com.jewan.learnspringframework.mockito.mockitodemo.list;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class ListTest {
+
+    @Test
+    void simpleTest() {
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(3);
+        assertEquals(3, listMock.size());
+    }
+
+    @Test
+    void multipleReturns() {
+        List listMock = mock(List.class);
+        when(listMock.size()).thenReturn(1).thenReturn(2); // 여러 개를 반환하면 마지막 값만 저장
+        assertEquals(1, listMock.size());
+        assertEquals(2, listMock.size());
+        assertEquals(2, listMock.size());
+        assertEquals(2, listMock.size());
+    }
+
+    @Test
+    void specificParameters() { // 특정 파라미터를 호출할 때
+        List listMock = mock(List.class);
+        when(listMock.get(0)).thenReturn("SomeString");
+        assertEquals("SomeString", listMock.get(0));
+        assertEquals(null, listMock.get(1));
+    }
+
+    @Test
+    void genericParameters() {
+        List listMock = mock(List.class);
+        when(listMock.get(Mockito.anyInt())).thenReturn("SomeOtherString");
+        assertEquals("SomeOtherString", listMock.get(0));
+        assertEquals("SomeOtherString", listMock.get(1));
+        assertEquals("SomeOtherString", listMock.get(1120));
+    }
+}
