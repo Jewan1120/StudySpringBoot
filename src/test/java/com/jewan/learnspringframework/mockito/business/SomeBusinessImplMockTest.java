0fbd54c5597a3a8c304ed2a70210be294b59cdae
Jewan1120 <hawaii1468@gmail.com>
2024-09-02 01:09:20 +0900

    [test] Mock으로 단위 테스트 - 생성 / 모킹 / 주입

---
+++ src/test/java/com/jewan/learnspringframework/mockito/business/SomeBusinessImplMockTest.java
@@ -0,0 +1,33 @@
+package com.jewan.learnspringframework.mockito.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class) // Mock테스트 확장
+class SomeBusinessImplMockTest {
+
+    @Mock // Mock 생성
+    private DataService dataServiceMock;
+
+    @InjectMocks // Mock 주입
+    private SomeBusinessImpl businessImpl;
+
+    @Test
+    void findTheGreatestFromAllData_basic() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 25, 15, 5 });
+        assertEquals(25, businessImpl.findTheGreatestFromAllData());
+    }
+
+    @Test
+    void findTheGreatestFromAllData_OneValue() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 35 });
+        assertEquals(35, businessImpl.findTheGreatestFromAllData());
+    }
+
+}
